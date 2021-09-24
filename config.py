332e51a443c09a5c3ead31ebe1c989a56d97a7c8
kevaf <kevinkipyegon20@gmail.com>
2021-09-24 17:38:14 +0300

    made changes to config file

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
 import os
+from re import DEBUG
 class Config:
     '''
     General configuration parent class
     '''
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kev2214@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = 'kevoneverything'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+     #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'kevohronoh@gmail.com'
+    MAIL_PASSWORD = 'alicerono2214'
 
 class ProdConfig(Config):
     '''
@@ -12,6 +23,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     ''' 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     '''
@@ -24,10 +36,13 @@ class DevConfig(Config):
     DEBUG = True
 
 class TestConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kev2214@localhost/blog'
 
 class DevConfig(Config):
-    DEBUG = True
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kev2214@localhost/blog'
+    
+    DEBUG=True
 
 config_options = {
 'development':DevConfig,
