3095f7f9544eb96b7338bdcc6f0df75d48eb20cb
kevaf <kevinkipyegon20@gmail.com>
2021-09-25 00:27:29 +0300

    added views for all templates

--- app/main/froms.py
+++ app/main/froms.py
@@ -5,3 +5,18 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Bio.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField('Comment')
+
+
+class SubscriberForm(FlaskForm):
+    email = StringField('Your Email Address')
+    name = StringField('Enter your name',validators = [Required()])
+    submit = SubmitField('Subscribe')
