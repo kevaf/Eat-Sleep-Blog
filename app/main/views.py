1e0dc2ba441f56599a0f666d5048feddde1ed24b
kevaf <kevinkipyegon20@gmail.com>
2021-09-24 20:11:46 +0300

    added views for home page

--- app/main/views.py
+++ app/main/views.py
@@ -2,4 +2,22 @@ from flask import render_template, request, redirect, url_for,abort,flash
 from . import main
 from .froms import UpdateProfile
 from .. import db, photos
-from ..models import User
+from ..models import User, Comment, Blog, Subscriber
+from flask_login import login_required, current_user
+from ..requests import get_blogQuotes
+from ..email import mail_message
+
+#views
+@main.route('/')
+def index():
+    """
+    view root page function that returns index page
+    """
+
+@main.route('/home')
+def home():
+    blogQuote = get_blogQuotes()
+
+    title = 'Eat-Sleep_Blog'
+    return render_template('home.html', title=title, blogQuote=blogQuote)
+    
