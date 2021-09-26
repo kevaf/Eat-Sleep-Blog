e3399389885746229a12cef450490eafc0adfd21
kevaf <kevinkipyegon20@gmail.com>
2021-09-26 12:33:10 +0300

    deployment to heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "pitches login"
+    title = "Blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -27,7 +27,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to ExhibitPitch","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to EatSleepBlog","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
